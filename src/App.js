211f21af05a9ec78608a85c69b7e182b00296cab
Cosmin <co5myn92@yahoo.com>
2020-07-27 20:44:13 +0300

    Async/Await in React

--- src/App.js
+++ src/App.js
@@ -48,19 +48,19 @@ const App = () => {
     isError: false,
   });
 
-  const handleFetchStories = useCallback(() => {
+  const handleFetchStories = useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    axios
-    .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
+    try {
+      const result = await axios.get(url);
 
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
   }, [url]);
 
   useEffect(() => {
